614947bd674b2aec397b5e38b51439bce65faa7a
Klingert, Luca <luca.klingert@student.fhws.de>
2022-07-29 13:37:18 +0200

    initial

---
+++ backend/UserService.java
@@ -0,0 +1,31 @@
+
+
+import utils.BasicAuthHelper;
+import utils.BearerAuthHelper;
+import utils.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path( "users" )
+public class UserService
+{
+    @Context
+    private HttpServletRequest request;
+
+    @GET
+    @Path( "me" )
+    @Produces( { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML } )
+    public Response getPersons( )
+    {
+        final User user = BasicAuthHelper.accessControl( request );
+        final String token = BearerAuthHelper.createToken( user.getId( ) );
+
+        return Response.ok( token ).build( );
+    }
+}

0569ed2a0dd8f01b765982f597bac7d209fabae4
Klingert, Luca <luca.klingert@student.fhws.de>
2022-07-30 17:56:33 +0200

    stuff

---
+++ src/main/webapp/frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+        <loginComponent/>
+      </div>
+  </div>
+</template>
+
+<script>
+
+import loginComponent from "@/components/main/loginComponent";
+
+export default {
+  name: 'App',
+  components: {
+    loginComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 250px;
+}
+
+.wrapper {
+  left: 50%;
+}
+
+</style>
